32038c8073db86675d79c13cab77364aa481aa15
sang <111@qq.com>
2017-10-17 20:14:02 +0800

    222

---
+++ test2/src/org/sang/Hello.java
@@ -0,0 +1,22 @@
+package org.sang;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Lenovo on 2017/10/16.
+ */
+public class Hello  extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("helloworld");
+    }
+}
